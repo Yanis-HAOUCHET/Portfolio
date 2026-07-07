14b496cea01f4523b7be50d83a06773afbe201c1
Yanis-HAOUCHET <97682673+Yanis-HAOUCHET@users.noreply.github.com>
2026-07-07 09:32:46 +0100

    Refactor script.js for better readability and functionality NEW JS
    Updated ScrollReveal targets and removed unused code.
    
    Signed-off-by: Yanis-HAOUCHET <97682673+Yanis-HAOUCHET@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -45,23 +45,6 @@ navbar.classList.remove('active');
 
 };
 
-//swiper script//
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
   /*===================*/
 
   let darkModeIcon = document.querySelector('#darkMod-icon');
@@ -82,6 +65,6 @@ var swiper = new Swiper(".mySwiper", {
   });
 
   ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-  ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper,.contact form', { origin: 'bottom' });
+  ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .featured-project, .timeline-item, .contact form', { origin: 'bottom' });
   ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
   ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
